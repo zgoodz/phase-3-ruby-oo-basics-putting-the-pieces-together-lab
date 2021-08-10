310cdd0467978703ff26134e98e60adabec743d6
Zachary Goodman <znote89@gmail.com>
2021-08-10 17:05:05 -0400

    commit tree 28d90ea5e70e6d0f32b01fb038e0198ec8ca7a20

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+require "pry"
+
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        if @condition == "old" 
+            @condition = "new"
+        end
+    end
+end
